bd41f0cbcd72f84ea2526f5249ff370d7e67b8e4
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2025-08-11 00:29:34 +0530

    Delete CreateViewForPowerBIsql.sql

--- CreateViewForPowerBIsql.sql
+++
@@ -1,8 +0,0 @@
--- Create View for Power BI
-CREATE View vw_ChurnData as
-select * from production_churn where Customer_Status in ('Churned', 'Stayed')
-GO
-
-CREATE View vw_JoinData as
-select * from production_churn where Customer_Status = 'Joined'
-GO
